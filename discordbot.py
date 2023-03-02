41ccfb5cc05b28095029600d2743fbcb1fc9fe03
KarioLee <80681340+KarioLee@users.noreply.github.com>
2023-03-02 20:12:33 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -28,8 +28,8 @@ service = discovery.build(
 reactRange = RoleSheetName + '!A2:B'
 rangeName = ReplySheetName + '!A2:D'
 
-intent=discord.Intents.all()
-client = commands.Bot(command_prefix = "!",intents=intent)
+intents=discord.Intents.all()
+client =discord.Client(intents=intents)
 
 
 
